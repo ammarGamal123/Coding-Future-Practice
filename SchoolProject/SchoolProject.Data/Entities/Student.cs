96b70eace282c6d4e21edf6223a2af4d2d531fdf
Ammar Hammad <amargamal832909@gmail.com>
2024-06-19 21:11:51 +0300

    Add SchoolProject to remote repository

---
+++ SchoolProject/SchoolProject.Data/Entities/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Address { get; set; }
+
+        [StringLength(25)]
+        public string Phone { get; set; }
+
+
+        [ForeignKey("DepartmentID")]
+        [InverseProperty("Students")]
+        public int DepartmentID { get; set; }
+
+
+        public virtual Department Departments { get; set; } 
+    }
+}
