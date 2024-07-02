dd534e73b9971b6ac55608c8d0912d73d16ea754
Ammar Hammad <amargamal832909@gmail.com>
2024-07-02 19:46:44 +0300

    feat: Add database connection (ApplicationDbContext) in infrastructure layer and implement entities in data layer

--- Coding-Future-Practice/CleanArchitecture/SchoolProject/SchoolProject.Data/Entities/Department.cs
+++ Coding-Future-Practice/CleanArchitecture/SchoolProject/SchoolProject.Data/Entities/Department.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SchoolProject.Data.Entities
 {
-    public class Department
+    public partial class Department
     {
+        public Department()
+        {
+            Students = new HashSet<Student>();
+            DepartmentSubjects = new HashSet<DepartmentSubject>();
+        }
+        [Key]
+        public int DeptID { get; set; }
+
+        [StringLength(500)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+
+        public virtual ICollection<DepartmentSubject> DepartmentSubjects { get; set;}
+
     }
 }
